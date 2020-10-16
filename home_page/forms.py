8e0e748b8eeb232e5aaf32877ce52fc30bc1741d
diogenes <diogenes.araujo.palla@gmail.com>
2020-10-16 19:43:32 -0300

    alguns testes com o formulario

--- home_page/forms.py
+++ home_page/forms.py
@@ -3,10 +3,45 @@ from django.core.mail.message import EmailMessage
 
 
 class ContatoForm(forms.Form):
-    nome = forms.CharField(label='Nome', max_length=100)
-    email = forms.EmailField(label='Email', max_length=100)
-    assunto = forms.CharField(label='Assunto', max_length=100)
-    mensagem = forms.CharField(label='Mensagem', widget=forms.Textarea())
+    nome = forms.CharField(
+        label='Nome',
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Seu nome completo'
+            }
+        )
+    )
+    email = forms.EmailField(
+        label='Email',
+        max_length=100,
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Digite seu email'
+            }
+        )
+    )
+    assunto = forms.CharField(
+        label='Assunto',
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Assunto'
+            }
+        )
+    )
+    mensagem = forms.CharField(
+        label='Mensagem',
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Assunto'
+            }
+        )
+    )
 
     def send_mail(self):
         nome = self.cleaned_data['nome']
