fa35ea68dfd6fb8a26c1185df9739658ce139b53
diogenes <diogenes.araujo.palla@gmail.com>
2020-10-17 15:05:29 -0300

    Adicionar dotenv para implementar segurança do django nao expondo as chaves de criptografia

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(5lujnt89ns=n&615)j+9j$za98%43qk5xl@w@#%i1i5i(#np#'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
